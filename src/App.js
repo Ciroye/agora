bf4f90ccf4974be8c3c4f6cebb15f133120e35d5
ciroye <jmcirot@unal.edu.co>
2020-05-17 23:45:46 -0500

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'
-=======
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
->>>>>>> 1bb1439b4e7c08d403368f540ec74846542f3afb
 import Home from "./views/home";
 import Meet from "./views/meet";
 
